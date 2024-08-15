d3d47b5ef04b458a9088f69ace1200686e7467b7
baiqinghui <baiqinghui1@tal.com>
2024-08-15 18:59:11 +0800

    将数组转换成树

---
+++ js手写/18.将数组转换成树.js
@@ -0,0 +1,25 @@
+function convert(arr) {
+    const res = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] !== 'object' || arr[i] == null) {
+            res.push({ 
+                value: arr[i] 
+            })
+        } else if (Array.isArray(arr[i])) {
+            // 
+            res.push({ 
+                children: convert(arr[i]) 
+            })
+        }
+    }
+
+    return res
+}
+
+
+// 测试数组
+const arr = [1, 2, 3, [4, 5]];
+
+let o = convert(arr);
+console.log(o);
